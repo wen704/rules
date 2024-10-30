a86eb5d782f2bc4d51f568822e39f6dc43096c04
wen704 <wen704889685@163.com>
2024-10-30 10:26:06 +0800

    chore maotai

--- js/maotai.js
+++ js/maotai.js
@@ -4,7 +4,7 @@
  * @supported Quantumult X (v1.0.5-build188)
  *
  * [rewrite_local]
- * ^https://.*.moutai519.com.cn/ url script-request-header sample-rewrite-request-header.js
+ * ^https?:\/\/.*?\.moutai519\.com\.cn/.* url script-request-header sample-rewrite-request-header.js
  */
 
 var modifiedHeaders = $request.headers;
@@ -13,4 +13,4 @@ modifiedHeaders['MT-APP-Version'] = '1.7.3';
 $done({headers : modifiedHeaders});
 
 
-//^https\://.*?.moutai519.com.cn/ url request-header (\r\n)MT-APP-Version:.+(\r\n) request-header $1MT-APP-Version: 1.7.3$2
+//^https?:\/\/.*?\.moutai519\.com\.cn/.* url request-header (\r\n)MT-APP-Version:.+(\r\n) request-header $1MT-APP-Version: 1.7.3$2
