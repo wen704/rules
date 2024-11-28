28c1ee282ed5a3c4cb4eb24bf9bde94a3818510b
wen704 <wen704889685@163.com>
2024-11-28 13:03:18 +0800

    chore ftms

--- js/ftms.js
+++ js/ftms.js
@@ -9,10 +9,11 @@ if (url.includes("app-bff/v1/version/latestupdate")) {
 } else if (url.includes("ftms-iov-app-user/user/getUserInfo")) {
   obj.result.userDefaultCarInfo.realnameStatus = "1";
 } else if (url.includes("ftms-iov-app-gbook/carFunction/config/getCarFunctionConfig")) {
-  obj.result.serviceAvailability.dkey = {
-    "status": "1",
-    "availability": "1"
-  };
+  // 数字钥匙,没啥用
+  // obj.result.serviceAvailability.dkey = {
+  //   "status": "1",
+  //   "availability": "1"
+  // };
   obj.result.remoteControll = {
     "doorlockcancel": "1",
     "doorlock": "1",
