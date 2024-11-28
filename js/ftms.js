948bcec8c92fd24e28f0300d190b4c28ee81535a
wen704 <wen704889685@163.com>
2024-11-28 12:59:27 +0800

    chore ftms

--- js/ftms.js
+++ js/ftms.js
@@ -6,6 +6,21 @@ if (url.includes("app-bff/v1/version/latestupdate")) {
   obj.data = {};
 } else if (url.includes("ftms-iov-app-car/tlmp/getCarCertificationInfo")) {
   obj.result.realnameStatus = "1";
+} else if (url.includes("ftms-iov-app-user/user/getUserInfo")) {
+  obj.result.userDefaultCarInfo.realnameStatus = "1";
+} else if (url.includes("ftms-iov-app-gbook/carFunction/config/getCarFunctionConfig")) {
+  obj.result.serviceAvailability.dkey = {
+    "status": "1",
+    "availability": "1"
+  };
+  obj.result.remoteControll = {
+    "doorlockcancel": "1",
+    "doorlock": "1",
+    "hazard": "1",
+    "carfinder": "1",
+    "window": "1",
+    "remotestart": "1"
+  };
 }
 
 $done({ body: JSON.stringify(obj) });
